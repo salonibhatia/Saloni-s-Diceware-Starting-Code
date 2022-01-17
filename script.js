104fb34ecfe4dfe410d07c87e6ce61be92d99f73
salonib <90460445+salonibhatia@users.noreply.github.com>
2022-01-17 20:11:50 +0000

    I added the reflection and made changes to the script file

--- script.js
+++ script.js
@@ -24,7 +24,14 @@ function preload() {
  let oneKey = "53122"
  let anotKey = "51442"
  let somKey = "45652"
- 
+ let bKey = "11141"
+ let cKey = "11245"
+ let dKey = "12252"
+ let eKey = "12612"
+let fKey = "14213" 
+let gKey = "13123"
+let hKey = "12441"
+let iKey = "53654"
 function generateLookupKey() {
   var length = 6;
   key = "";
@@ -51,14 +58,21 @@ function setup() {
 }
 
 function draw() {
-  background("red");
+  background("peachpuff");
  text(words.get(lookupKey), width/15, height/3);
  text(words.get(searchKey), width/6, height/3);
 text(words.get(aKey), width/3, height/3);
 text(words.get(oneKey), width/4, height/2);
 text(words.get(anotKey), width/7, height/2);
 text(words.get(somKey), width/15, height/2);
-
+text(words.get(bKey), width/7, height/5);
+text(words.get(cKey), width/7, height/9)
+text(words.get(dKey), width/9, height/3)
+text(words.get(eKey), width/3, height/11)
+text(words.get(fKey), width/5, height/4)
+text(words.get(gKey), width/4, height/9)
+text(words.get(hKey), width/11, height/7)
+text(words.get(iKey), width/16, height/9)
 
 }
 
@@ -67,12 +81,51 @@ function keyPressed() {
      lookupKey = generateLookupKey();
      searchKey = generateLookupKey();
      aKey = generateLookupKey();
-     oneKey = generateLookupKey();
+     
+     
+   }
+   while(key=="2"){
+     bKey = generateLookupKey();
      anotKey = generateLookupKey();
      somKey = generateLookupKey();
+    
+     
    }
+   while(key == "3"){
+     dKey = generateLookupKey();
+     oneKey = generateLookupKey();
+    cKey = generateLookupKey();
+   }
+   while(key == "4"){
+dKey = generateLookupKey();
+eKey = generateLookupKey();
+fKey = generateLookupKey();
+  }
+  while(key=="5"){
+    gKey= generateLookupKey();
+    hKey = generateLookupKey();
+    iKey = generateLookupKey();
+  }
+  while(key== "0"){
+ lookupKey = generateLookupKey();
+     searchKey = generateLookupKey();
+     aKey = generateLookupKey();
+      bKey = generateLookupKey();
+     anotKey = generateLookupKey();
+     somKey = generateLookupKey();
+     dKey = generateLookupKey();
+     oneKey = generateLookupKey();
+    cKey = generateLookupKey();
+    dKey = generateLookupKey();
+  eKey = generateLookupKey();
+  fKey = generateLookupKey();
+    gKey= generateLookupKey();
+     hKey = generateLookupKey();
+    iKey = generateLookupKey();
+  }
 
 }
+
 
 
 
